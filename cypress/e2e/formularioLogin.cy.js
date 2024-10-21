1c89d2311f5fde1c9230276c441f186f79d009c8
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-10-20 22:50:44 -0300

    :test_tube: test: adiciona validação para campo de email em branco no formulário de login

--- cypress/e2e/formularioLogin.cy.js
+++ cypress/e2e/formularioLogin.cy.js
@@ -13,4 +13,14 @@ describe('Formulário de Login', () => {
             .should('exist')
             .and('have.text', 'O email digitado é inválido')
     })
+
+    it('Não deve permitir um campo em branco', () => {
+        cy.getByData('botao-login').click()
+        cy.getByData('senha-input').type('Senha123123')
+        cy.getByData('botao-enviar').click()
+
+        cy.getByData('mensagem-erro')
+            .should('exist')
+            .and('have.text', 'O campo email é obrigatório')
+    })
 })
