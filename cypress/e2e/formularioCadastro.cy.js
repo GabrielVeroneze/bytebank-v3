b7d6a516217cf2c2d7e50afbdbfa0048ae11b730
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-10-21 23:29:52 -0300

    :test_tube: test: adiciona verificação para campo de nome obrigatório no formulário de cadastro

--- cypress/e2e/formularioCadastro.cy.js
+++ cypress/e2e/formularioCadastro.cy.js
@@ -15,4 +15,15 @@ describe('Formulário de Cadastro', () => {
             .should('exist')
             .and('have.text', 'Usuário cadastrado com sucesso!')
     })
+
+    it('Não deve permitir o cadastro de usuários sem o campo nome preenchido', () => {
+        cy.getByData('botao-cadastro').click()
+        cy.getByData('email-input').type('exemplo@email.com')
+        cy.getByData('senha-input').type('Senha123123')
+        cy.getByData('botao-enviar').click()
+
+        cy.getByData('mensagem-erro')
+            .should('exist')
+            .and('have.text', 'O campo de nome é obrigatório')
+    })
 })
